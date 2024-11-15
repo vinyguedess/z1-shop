f25fb31ba43d842bae4852dce93de73d3dcbff4e
Vinicius Guedes <viniciusgued@gmail.com>
2024-11-15 12:46:54 -0300

    only admin can create a product

--- app/controllers/products_controller.ts
+++ app/controllers/products_controller.ts
@@ -1,6 +1,7 @@
 import ProductsService from '#services/products_service'
 import type { HttpContext } from '@adonisjs/core/http'
 import { inject } from '@adonisjs/core'
+import Product from '#models/product'
 
 @inject()
 export default class ProductsController {
@@ -13,7 +14,7 @@ export default class ProductsController {
    * @responseBody 200 - <Product[]>
    * @responseHeader 200 - X-Total-Count - Total of items registered - @example(10)
    */
-  async index(ctx: HttpContext) {
+  async index(ctx: HttpContext): Promise<Array<Product>> {
     const [results, totalResults] = await this.productService.getList(
       ctx.request.qs().limit || 10,
       ctx.request.qs().page || 1
@@ -30,7 +31,14 @@ export default class ProductsController {
    * @responseBody 201
    * @responseHeader 201 - ETag - ID of newly created product
    */
-  async create(ctx: HttpContext) {
+  async create(ctx: HttpContext): Promise<void> {
+    const user = ctx.auth.use('jwt').getUserOrFail()
+    if (!user.isAdmin)
+      return ctx.response.forbidden({
+        code: 'YOU_DONT_HAVE_PERMISSION',
+        message: "You don't have permission to create a product",
+      })
+
     const product = await this.productService.create(ctx.request.body())
     ctx.response.header('ETag', product.id)
     ctx.response.status(201)
--- tests/functional/controllers/products_controller.spec.ts
+++ tests/functional/controllers/products_controller.spec.ts
@@ -1,10 +1,13 @@
 import ProductsController from '#controllers/products_controller'
 import Product from '#models/product'
 import ProductsService from '#services/products_service'
+import { AuthenticatorClient } from '@adonisjs/auth'
 import app from '@adonisjs/core/services/app'
 import testUtils from '@adonisjs/core/services/test_utils'
 import { test } from '@japa/runner'
 import sinon from 'sinon'
+import { JwtGuard } from '../../../app/auth/guards/jwt_guard.js'
+import User from '#models/user'
 
 test('index', async ({ assert }) => {
   const stubGetList = sinon.stub(ProductsService.prototype, 'getList')
@@ -20,19 +23,74 @@ test('index', async ({ assert }) => {
   sinon.restore()
 })
 
-test('create', async ({ assert }) => {
-  const product = new Product()
-  product.id = 1
+test.group('create', () => {
+  test('ok', async ({ assert }) => {
+    const product = new Product()
+    product.id = 1
 
-  const stubCreate = sinon.stub(ProductsService.prototype, 'create')
-  stubCreate.resolves(product)
+    const stubCreate = sinon.stub(ProductsService.prototype, 'create')
+    stubCreate.resolves(product)
 
-  const ctx = await testUtils.createHttpContext()
+    const user = new User()
+    user.isAdmin = true
 
-  const productsController = await app.container.make(ProductsController)
-  await productsController.create(ctx)
+    const stubJwtGuard = sinon.createStubInstance(JwtGuard)
+    stubJwtGuard.getUserOrFail.returns(user)
 
-  assert.strictEqual(ctx.response.getStatus(), 201)
-  assert.strictEqual(ctx.response.getHeader('ETag'), '1')
-  sinon.assert
+    const authClient = new AuthenticatorClient({
+      default: 'jwt',
+      guards: {
+        jwt: () => stubJwtGuard,
+      },
+    })
+
+    const ctx = await testUtils.createHttpContext()
+    ctx.auth = authClient
+
+    const productsController = await app.container.make(ProductsController)
+    await productsController.create(ctx)
+
+    assert.strictEqual(ctx.response.getStatus(), 201)
+    assert.strictEqual(ctx.response.getHeader('ETag'), '1')
+    sinon.assert.calledWith(stubJwtGuard.getUserOrFail)
+
+    sinon.restore()
+  })
+
+  test('if user is not admin', async ({ assert }) => {
+    const product = new Product()
+    product.id = 1
+
+    const stubCreate = sinon.stub(ProductsService.prototype, 'create')
+    stubCreate.resolves(product)
+
+    const user = new User()
+    user.isAdmin = false
+
+    const stubJwtGuard = sinon.createStubInstance(JwtGuard)
+    stubJwtGuard.getUserOrFail.returns(user)
+
+    const authClient = new AuthenticatorClient({
+      default: 'jwt',
+      guards: {
+        jwt: () => stubJwtGuard,
+      },
+    })
+
+    const ctx = await testUtils.createHttpContext()
+    ctx.auth = authClient
+
+    const productsController = await app.container.make(ProductsController)
+    await productsController.create(ctx)
+
+    assert.strictEqual(ctx.response.getStatus(), 403)
+    assert.deepEqual(ctx.response.getBody(), {
+      code: 'YOU_DONT_HAVE_PERMISSION',
+      message: "You don't have permission to create a product",
+    })
+    sinon.assert.calledWith(stubJwtGuard.getUserOrFail)
+    sinon.assert.notCalled(stubCreate)
+
+    sinon.restore()
+  })
 })
