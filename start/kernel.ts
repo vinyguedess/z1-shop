ecd6edbfd3bf3f589976fa6a3a1d367a5c02cba9
Vinicius Guedes <viniciusgued@gmail.com>
2024-11-15 01:33:45 -0300

    adds missing auth setup

--- start/kernel.ts
+++ start/kernel.ts
@@ -40,4 +40,6 @@ router.use([() => import('@adonisjs/auth/initialize_auth_middleware')])
  * Named middleware collection must be explicitly assigned to
  * the routes or the routes group.
  */
-export const middleware = router.named({})
+export const middleware = router.named({
+  auth: () => import('#middleware/auth_middleware'),
+})
