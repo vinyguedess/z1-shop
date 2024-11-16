62853579b121aa361bf61338bce3e2fa39198675
Vinicius Guedes <viniciusgued@gmail.com>
2024-11-16 02:49:13 -0300

    query for creating order item

--- app/repositories/orders_repository.ts
+++ app/repositories/orders_repository.ts
@@ -1,8 +1,13 @@
 import Order from '#models/order'
+import OrderItem from '#models/order_item'
 import { TransactionClientContract } from '@adonisjs/lucid/types/database'
 
 export default class OrdersRepository {
   create(trx: TransactionClientContract, data: Record<string, any>): Promise<Order> {
     return new Order().fill(data).useTransaction(trx).save()
+  }
+
+  createOrderItem(trx: TransactionClientContract, data: Record<string, any>): Promise<OrderItem> {
+    return new OrderItem().fill(data).useTransaction(trx).save()
   }
 }
--- tests/functional/repositories/orders_repository.spec.ts
+++ tests/functional/repositories/orders_repository.spec.ts
@@ -1,4 +1,5 @@
 import Order from '#models/order'
+import OrderItem from '#models/order_item'
 import OrdersRepository from '#repositories/orders_repository'
 import app from '@adonisjs/core/services/app'
 import db from '@adonisjs/lucid/services/db'
@@ -23,6 +24,28 @@ test('create', async ({ assert }) => {
   sinon.assert.calledWith(stubOrder.fill, {})
   sinon.assert.calledWith(stubOrder.useTransaction, stubTrx)
   sinon.assert.calledWith(stubOrder.save)
+
+  sinon.restore()
+})
+
+test('createOrderItem', async ({ assert }) => {
+  const txn = await db.transaction()
+  await txn.rollback()
+
+  const stubTrx = sinon.stub(txn)
+
+  const stubOrderItem = sinon.stub(OrderItem.prototype)
+  stubOrderItem.fill.returnsThis()
+  stubOrderItem.useTransaction.returnsThis()
+  stubOrderItem.save.returnsThis()
+
+  const ordersRepository = await app.container.make(OrdersRepository)
+  const response = await ordersRepository.createOrderItem(stubTrx, {})
+
+  assert.instanceOf(response, OrderItem)
+  sinon.assert.calledWith(stubOrderItem.fill, {})
+  sinon.assert.calledWith(stubOrderItem.useTransaction, stubTrx)
+  sinon.assert.calledWith(stubOrderItem.save)
 
   sinon.restore()
 })
