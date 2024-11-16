b3af320e466727bf97744f4731c0335faaa5ae2a
Vinicius Guedes <viniciusgued@gmail.com>
2024-11-16 14:42:19 -0300

    query for fetching users orders

--- app/repositories/orders_repository.ts
+++ app/repositories/orders_repository.ts
@@ -10,4 +10,13 @@ export default class OrdersRepository {
   createOrderItem(trx: TransactionClientContract, data: Record<string, any>): Promise<OrderItem> {
     return new OrderItem().fill(data).useTransaction(trx).save()
   }
+
+  async getListByUserId(userId: number, limit: number, offset: number): Promise<[Order[], number]> {
+    const query = Order.query().where('user_id', userId)
+
+    const results = await query.limit(limit).offset(offset)
+
+    const countResults = await query.count('id', 'total_results')
+    return [results, countResults[0].$extras.total_results]
+  }
 }
--- tests/functional/repositories/orders_repository.spec.ts
+++ tests/functional/repositories/orders_repository.spec.ts
@@ -2,9 +2,10 @@ import Order from '#models/order'
 import OrderItem from '#models/order_item'
 import OrdersRepository from '#repositories/orders_repository'
 import app from '@adonisjs/core/services/app'
+import { ModelQueryBuilder } from '@adonisjs/lucid/orm'
 import db from '@adonisjs/lucid/services/db'
 import { test } from '@japa/runner'
-import sinon from 'sinon'
+import sinon, { SinonStubbedInstance } from 'sinon'
 
 test('create', async ({ assert }) => {
   const txn = await db.transaction()
@@ -48,4 +49,20 @@ test('createOrderItem', async ({ assert }) => {
   sinon.assert.calledWith(stubOrderItem.save)
 
   sinon.restore()
+})
+
+test('getListByUserId', async ({ assert }) => {
+  const stubQueryBuilder = sinon.createStubInstance(ModelQueryBuilder)
+  stubQueryBuilder.where.returnsThis()
+  stubQueryBuilder.limit.returnsThis()
+  stubQueryBuilder.offset.resolves([])
+  stubQueryBuilder.count.resolves([{ $extras: { total_results: 0 } }])
+
+  const stubModel = sinon.stub(Order) as SinonStubbedInstance<typeof Order>
+  stubModel.query.returns(stubQueryBuilder)
+
+  const ordersRepository = await app.container.make(OrdersRepository)
+  const response = await ordersRepository.getListByUserId(1, 10, 0)
+
+  assert.deepEqual(response, [[], 0])
 })
