0f2a5d85a9ddfc5b188f8501a8998c9807e461c7
Vinicius Guedes <viniciusgued@gmail.com>
2024-11-16 01:36:06 -0300

    migration script for creating orders and order_items table

---
+++ database/migrations/1731731090494_create_orders_table.ts
@@ -0,0 +1,27 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'orders'
+
+  async up() {
+    this.schema
+      .createTable(this.tableName, (table) => {
+        table.increments('id')
+        table.integer('user_id').unsigned().notNullable().references('users.id')
+        table.timestamp('created_at')
+        table.timestamp('updated_at')
+      })
+      .createTable('order_items', (table) => {
+        table.increments('id')
+        table.integer('order_id').unsigned().notNullable().references('orders.id')
+        table.integer('product_id').unsigned().notNullable().references('products.id')
+        table.decimal('price', 10, 2).notNullable()
+        table.timestamp('created_at')
+        table.timestamp('updated_at')
+      })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
